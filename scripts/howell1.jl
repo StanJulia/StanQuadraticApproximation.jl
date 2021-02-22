60a94a5f968b0dfe2ed3ee5dc9786d129d3e18ea
Rob J Goedman <goedman@mac.com>
2021-02-22 07:25:48 -0700

    Rel 0.1.0 - Test running on Github

--- scripts/howell1.jl
+++ scripts/howell1.jl
@@ -12,13 +12,13 @@ data {
   real<lower=0> h[N];
 }
 parameters {
-  real<lower=0> sigma;
-  real<lower=0,upper=250> mu;
+  real<lower=0.1> sigma;
+  real<lower=100,upper=180> mu;
 }
 model {
   // Priors for mu and sigma
   mu ~ normal(178, 20);
-  sigma ~ uniform(0 , 50);
+  sigma ~ exponential(1);
 
   // Observed heights
   h ~ normal(mu, sigma);
@@ -26,7 +26,28 @@ model {
 ";
 
 data = (N = size(df, 1), h = df.height)
-init = (mu = 178.0, sigma = 25.0)
+init = (mu = 160.0, sigma = 10.0)
 
 qm, sm, om = quap("s4.1s", stan4_1; data, init)
 qm |> display
+
+stan4_2 = "
+// Inferring the mean and std
+data {
+  int N;
+  real<lower=0> h[N];
+}
+parameters {
+  real<lower=0> sigma;
+  real<lower=0,upper=250> mu;
+}
+model {
+  // Priors for mu and sigma
+  mu ~ normal(178, 20);
+  sigma ~ exponentail(1);
+
+  // Observed heights
+  h ~ normal(mu, sigma);
+}
+";
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,4 @@
 using StanQuap
 using Test
+
+include("./../scripts/howell1.jl")
